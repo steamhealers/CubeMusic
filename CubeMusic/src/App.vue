9bd8415d089b0a49064481f1eac589ae37eabfac
SteamHealer <370349055@qq.com>
2017-12-16 23:00:56 +0800

    environment bulid
    环境搭建

---
+++ CubeMusic/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+  export default {
+
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ CubeMusic/src/main.js
@@ -0,0 +1,23 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import App from './App'
+
+import fastclick from 'fastclick'
+// import 'common/stylus/index.styl'
+
+/* eslint-disable no-unused-vars */
+// import vConsole from 'vconsole'
+
+fastclick.attach(document.body)
+
+// Vue.use(VueLazyload, {
+//   loading: require('common/image/default.png')
+// })
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  // router,
+  // store,
+  render: c => c(App)
+})
